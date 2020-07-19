6677ad7813752b739bf8e2b4a1bf0c9a7e3160fd
Christoffer Nordenlöw <63145499+cnordenlow@users.noreply.github.com>
2020-07-19 12:42:16 +0200

    Create 05.HTML_CSS_JS.R

---
+++ 05.HTML_CSS_JS.R
@@ -0,0 +1,41 @@
+# HTML, CSS and JAVASCRIPT
+
+
+## HTML
+  
+### Bond Calculator in R
+
+#### Calculate Forward Rates
+
+**Calculate Forward Rates**
+  
+
+```{r, echo=T, eval=F}
+
+``` 
+
+## CSS
+  
+### Bond Calculator in R
+
+#### Calculate Forward Rates
+
+**Calculate Forward Rates**
+  
+
+```{r, echo=T, eval=F}
+
+``` 
+
+## JAVASCRIPT
+
+### Bond Calculator in R
+
+#### Calculate Forward Rates
+
+**Calculate Forward Rates**
+  
+
+```{r, echo=T, eval=F}
+
+``` 
